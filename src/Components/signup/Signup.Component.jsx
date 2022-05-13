f556cabc066ff5b3411beca92e37b13fae96997e
rajni <sainirajni292000@gmail.com>
2022-05-13 15:54:03 +0530

    commit

--- src/Components/signup/Signup.Component.jsx
+++ src/Components/signup/Signup.Component.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import signup from './Signup.style.css'
+import './Signup.style.css'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+// import  Link  from 'react-router-dom';
 
 function Signup() {
     const [data, setData] = useState({
@@ -9,7 +9,6 @@ function Signup() {
         email: "",
         adress: "",
         password: "",
-
         confirmPassword: '',
 
 
@@ -23,13 +22,14 @@ function Signup() {
 
     const signupHandler = e => {
         e.preventDefault();
-        // console.log(data);
+        // console.log(data)
         axios.post('http://localhost:8080/api/users/register', {
-            name: username,
+            username: username,
             email: email,
             adress: adress,
             password: password,
             confirmPassword: confirmPassword
+           
         })
         .then(res => console.log(res))
         .catch(err => console.log(err));
@@ -42,7 +42,7 @@ function Signup() {
                     
                     <form onSubmit={signupHandler}>
 
-                        <input type="text" name="name" placeholder="UserName" value={username} onChange={changeHandler} /><br />
+                        <input type="text" name="username" placeholder="UserName" value={username} onChange={changeHandler} /><br />
                         <input type="email" name="email" placeholder="UserEmail" value={email} onChange={changeHandler} /><br />
                         <input type="text" name="adress" placeholder="UserAddress" value={adress} onChange={changeHandler} /><br />
                         <input type="password" name="password" placeholder="UserPassword" value={password} onChange={changeHandler} /><br />
